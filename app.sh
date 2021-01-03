d3fd9ca4f2e9a0cb2f0c11d4c6e6a228ffad6bd1
Victor Gabriel <victorsnt123@gmail.com>
2021-01-03 13:25:54 -0300

    adding aditional configs to project

---
+++ app.sh
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=prod target/players-search-0.0.1-SNAPSHOT.jar --JAWSDB_URL=mysql://ls58d7629u9opgbw:t2xbe2pqzx2ygkgy@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vptpznmyptdev02p --JAWSDB_USER=ls58d7629u9opgbw --JAWSDB_PASSWORD=t2xbe2pqzx2ygkgy
